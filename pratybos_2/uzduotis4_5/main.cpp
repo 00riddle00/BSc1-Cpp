cf7bdbf68392685fffacc8d1c2a5400292123425
00riddle00 <tomasgiedraitis@gmail.com>
2018-02-15 18:47:33 +0200

    Add uzduotis4-5 (stack dynamic list template class)

---
+++ pratybos_2/uzduotis4_5/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "mystack.h"
+#include "automobilis.h"
+using namespace std;
+
+
+int main() {
+
+    Automobilis automobilis1;
+    automobilis1.setGamintojas("BMW");
+    automobilis1.setModelis("520d");
+    automobilis1.setVariklioTuris(3.0);
+
+    Automobilis automobilis2;
+    automobilis2.setGamintojas("Audi");
+    automobilis2.setModelis("A4");
+    automobilis2.setVariklioTuris(2.0);
+
+    //vector<Automobilis> v;
+
+    //v.push_back(automobilis1);
+    //v.push_back(automobilis2);
+
+     //for(int i = 0; i < v.size(); i++) {
+        //v[i].print();
+     //}
+     
+/*    Stack <Automobilis> s1;*/
+
+    //s1.init();
+    //s1.push(automobilis1);
+    //s1.push(automobilis2);
+    //s1.printStack();
+    //s1.pop();
+    //s1.printStack();
+
+
+    Stack <float> s2;
+    s2.init();
+    s2.push(3.14);
+    s2.push(2.72);
+    s2.printStack();
+    s2.pop();
+    s2.printStack();
+
+
+    Stack <string> s3;
+    s3.init();
+    s3.push("Hello");
+    s3.push("World");
+    s3.printStack();
+    s3.pop();
+    s3.printStack();
+
+
+}
+
---
+++ pratybos_2/uzduotis4_5/mystack.h
@@ -0,0 +1,51 @@
+/* mystack.h -- Stack declaration and function prototypes:  */
+#ifndef MYSTACK_H
+#define MYSTACK_H
+#include <iostream>
+
+template <class T>
+class Stack {
+   public:
+        struct Node {
+            T data;
+            struct Node *link;
+        };
+
+       Node *top;
+
+        void init() {
+            this->top = NULL;
+        }
+
+        void push(T input) {
+          Node* temp = new Node();
+          temp->data = input;
+          temp->link = this->top;
+          this->top = temp;
+        }
+
+        void pop() {
+            Node *temp;
+            if(this->top == NULL) return;
+            temp = this->top;
+            this->top = this->top->link;
+            free(temp);
+        }
+
+        void printStack() {
+            Node *temp = this->top;
+            std::cout << "Stack elements from the top: " << std::endl;
+            int i = 1;
+            while (temp != NULL) {
+                std::cout << "[" << i << "] ";
+                std::cout << (temp->data) << " ";
+                //temp->data.print();
+                std::cout << std::endl;
+                temp = temp->link;
+                i++;
+            }
+            std::cout << std::endl;
+        }
+};
+
+#endif
