75cd40d1c729a1502404e37f07fd690315e77fc0
00riddle00 <tomasgiedraitis@gmail.com>
2018-04-07 23:02:11 +0300

    Add pratybos8 (in progress)

--- pratybos_7/uzduotis_3/TxtFailasRasymui.cpp
+++ pratybos_7/uzduotis_3/TxtFailasRasymui.cpp
@@ -1,206 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <streambuf>
 using namespace std;
 
 class TxtFailasRasymui : public ofstream {
     string fileName;
     ios_base::openmode mode;
 
-    TxtFailasRasymui(const string& file, ios_base::openmode mode = ios_base::out) : ofstream(file.c_str()) {
-        fileName = file;
-        // FIXME nebutina
-        mode = mode;
-    }
-
-};
-
-
-
-
-
-
-class Leidinys {
-    bool skolinamas;
-    bool paskolintas;
-
-    public:
-        Leidinys(bool skol = true, bool paskol = false) {
-            skolinamas = skol;
-            paskolintas = paskol;
-            cout << "Leidinys object is created" << endl;
-        }
-
-        ~Leidinys() {
-            cout << "Leidinys object is destroyed" << endl;
-        }
-
-        void change_skolinamas(bool skol) {
-            skolinamas = skol;
-        }
-
-        bool is_skolinamas() {
-            return skolinamas;
-        }
-
-        virtual void print_info() {}
- 
-        virtual void is_skolinamas_print() {
-            if (skolinamas) {
-                cout << "Leidinys yra skolinamas" << endl;
-            } else {
-                cout << "Leidynys nera skolinamas" << endl;
-            }
-        }
-
-        bool is_paskolintas() {
-            return paskolintas;
-        }
-
-        void paskolinti() {
-            if (skolinamas) {
-                paskolintas = true;
-            }
-        }
-
-        void grazinti() {
-            if (paskolintas) {
-                paskolintas = false;
-            }
-        }
-};
-
-class Knyga: public Leidinys {
-    string autorius;
-    string pavadinimas;
-    size_t leidimo_metai;
-
-    public:
-        Knyga(string aut = "NA", string pav = "NA", size_t metai=0, bool skol=true) {
-            autorius = aut;
-            pavadinimas = pav;
-            leidimo_metai = metai;
-            change_skolinamas(skol);
-            cout << "Knyga object is created" << endl;
-        }
-
-        ~Knyga() {
-            cout << "Knyga object is destroyed" << endl;
-        }
-
-        void print_info() {
-            cout << "Autorius: " << autorius << endl
-                << "Pavadinimas: " << pavadinimas << endl
-                << "Leidimo metai: " << leidimo_metai << endl;
-            if (is_skolinamas() && is_paskolintas()) {
-                cout << "Knyga yra paskolinta" << endl;
-            } else if (!is_skolinamas()) {
-                cout << "Knyga nera skolinama" << endl;
-            } else {
-                cout << "Knyga nera paskolinta" << endl;
-            }
-        }
-
-        void is_skolinamas_print() {
-            if (is_skolinamas()) {
-                cout <<"Knyga yra skolinama" << endl;
-            } else {
-                cout << "Knyga nera skolinama" << endl;
-            }
-        }
-
-};
-
-class AudioIrasas: public Leidinys {
-    string atlikejas;
-    string pavadinimas;
-    float trukme;
-    public:
-        AudioIrasas(string atl = "NA", string pav = "NA", float truk = 60.0) {
-            atlikejas = atl;
-            pavadinimas = pav;
-            trukme = truk;
-            cout << "AudioIrasas object is created" << endl;
-        }
-
-        ~AudioIrasas() {
-            cout << "AudioIrasas object is destroyed" << endl;
-        }
-
-        void print_info() {
-            cout << "Atlikejas: " << atlikejas << endl
-                << "Pavadinimas: " << pavadinimas << endl
-                << "Trukme: " << fixed << setprecision(1) << trukme <<  endl;
-
-            if (is_skolinamas() && is_paskolintas()) {
-                cout << "AudioIrasas yra paskolintas" << endl;
-            } else if (!is_skolinamas()) {
-                cout << "AudioIrasas nera skolinamas" << endl;
-            } else {
-                cout << "AudioIrasas nera paskolintas" << endl;
-            }
-        }
-
-        void is_skolinamas_print() {
-            if (is_skolinamas()) {
-                cout <<"AudioIrasas yra skolinamas" << endl;
-            } else {
-                cout << "AudioIrasas nera skolinamas" << endl;
-            }
-        }
-};
-
-
-class RetaKnyga: public Knyga {
-    void change_skolinamas(bool);
-    void paskolinti();
-    void grazinti();
-    void paskolintas();
-
-    public:
-        RetaKnyga(string aut = "NA", string pav = "NA", size_t metai=0) : Knyga(aut, pav, metai, false) {
-            cout << "RetaKnyga object is created" << endl;
-        }
-
-        ~RetaKnyga() {
-            cout << "RetaKnyga object is destroyed" << endl;
-        }
-
-        void is_skolinamas_print() {
-            cout <<"RetaKnyga yra neskolinama" << endl;
-        }
+    public: 
+        TxtFailasRasymui(const string& file, ios_base::openmode mode = ios_base::out) : ofstream(file.c_str()), fileName(file), mode(mode) { }
 
 };
 
 int main() {
+    TxtFailasRasymui out("file.txt");
 
-    size_t size_leidiniai = 3;
-    Leidinys* leidiniai[size_leidiniai] = {
-        new Knyga("A.Lindgren", "Broliai Liutasirdziai", 2015),
-        new AudioIrasas("Vytautas Kernagis", "Akustinis", 80.0),
-        new RetaKnyga("Claro Vascotti", "Institutiones Historiae Ecclesiasticae Novi Foederis", 1895)
-    };
-
-    cout << "---------------------------" << endl;
-    for (size_t i = 0; i < size_leidiniai; i++) {
-        leidiniai[i]->print_info();
-        leidiniai[i]->is_skolinamas_print();
-        cout << "---------------------------" << endl;
+    if (out.is_open()) {
+        cout << "File is open" << endl;
     }
-    cout << "---------------------------" << endl;
 
-    leidiniai[1]->paskolinti();
-    leidiniai[1]->print_info();
+    out << "Random text" << endl;
+    out << "Random text" << endl;
+    out << "Random text" << endl;
+    out << "Random text" << endl;
 
-    cout << "---------------------------" << endl;
+    out.close();
 
-    leidiniai[0]->paskolinti();
-    leidiniai[0]->print_info();
+/*    out.open("file.txt", ios::in);*/
 
-    cout << "---------------------------" << endl;
-    leidiniai[0]->grazinti();
-    leidiniai[0]->print_info();
+    //int count = 0;
+    //string word;
 
-    cout << "---------------------------" << endl;
+    //while (out >> word)
+        //count++;
+
+    /*cout << "word count is: " << count << endl;*/
+
 }
+
---
+++ pratybos_8/uzduotis1_ABC/Shapes.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+
+using namespace std;
+
+class GeomFigura {
+
+    public:
+        GeomFigura() {
+            cout << "GeomFigura is created" << endl;
+        }
+
+        virtual ~GeomFigura() {
+            cout << "GeomFigura is destroyed" << endl;
+        }
+
+        virtual void printInfo() {
+            cout << "GeomFigura" << endl;
+        }
+
+        virtual float plotas() const {
+            return 0.0;
+        } 
+
+        virtual float turis() const {
+            return 0.0;
+        }
+};
+
+
+class Figura2D : public GeomFigura {
+    float x_coordinate;
+    float y_coordinate;
+
+    public:
+        Figura2D(float x, float y) : x_coordinate(x), y_coordinate(y) {
+            cout << "Figura2D is created" << endl;
+        }
+
+        virtual ~Figura2D() {
+            cout << "Figura2D is destroyed" << endl;
+        }
+
+        void printCoordinates() {
+            cout << "Coordinates: x = " << fixed << setprecision(1) << x_coordinate
+                 << ", y = " << y_coordinate << endl;
+        }
+
+        virtual void printInfo() override {
+            cout << "Figura2D" << endl;
+        }
+};
+
+
+class Figura3D : public GeomFigura {
+    float x_coordinate;
+    float y_coordinate;
+    float z_coordinate;
+
+    public:
+        Figura3D(float x, float y, float z) : x_coordinate(x), y_coordinate(y), z_coordinate(z) {
+            cout << "Figura3D is created" << endl;
+        }
+
+        virtual ~Figura3D() {
+            cout << "Figura3D is destroyed" << endl;
+        }
+
+        void printCoordinates() {
+            cout << "Coordinates: x = " << fixed << setprecision(1) << x_coordinate
+                 << ", y = " << y_coordinate << ", z = " << z_coordinate << endl;
+        }
+
+        virtual void printInfo() override {
+            cout << "Figura2D" << endl;
+        }
+};
+
+
+class Kvadratas : public Figura2D {
+    float krastine;
+
+    public:
+        Kvadratas(float x, float y, float k) : Figura2D(x, y), krastine(k) {
+            cout << "Kvadratas is created" << endl;
+        }
+
+        ~Kvadratas() {
+            cout << "Kvadratas is destroyed" << endl;
+        }
+
+        virtual void printInfo() override {
+            cout << "Kvadratas, krastine = " << fixed << setprecision(1) << krastine << endl;
+            printCoordinates();
+        }
+
+        virtual float plotas() const override {
+            return pow(krastine, 2);
+        }
+};
+
+
+class Skritulys : public Figura2D {
+    float spindulys;
+
+    public:
+        Skritulys(float x, float y, float s) : Figura2D(x, y), spindulys(s) {
+            cout << "Skritulys is created" << endl;
+        }
+
+        ~Skritulys() {
+            cout << "Skritulys is destroyed" << endl;
+        }
+
+        virtual void printInfo() override {
+            cout << "Skritulys, spindulys = " << fixed << setprecision(1) << spindulys << endl;
+            printCoordinates();
+        }
+
+        virtual float plotas() const override {
+            return pow(M_PI * spindulys, 2);
+        }
+};
+
+
+class Kubas : public Figura3D {
+    float krastine;
+
+    public:
+        Kubas(float x, float y, float z, float k) : Figura3D(x, y, z), krastine(k) {
+            cout << "Kubas is created" << endl;
+        }
+
+        ~Kubas() {
+            cout << "Kubas is destroyed" << endl;
+        }
+
+        virtual void printInfo() override {
+            cout << "Kubas, krastine = " << fixed << setprecision(1) << krastine << endl;
+            printCoordinates();
+        }
+
+        virtual float plotas() const override {
+            return pow(krastine, 2) * 6;
+        }
+
+        virtual float turis() const override {
+            return pow(krastine, 3);
+        }
+};
+
+
+class Sfera : public Figura3D {
+    float spindulys;
+
+    public:
+        Sfera(float x, float y, float z, float s) : Figura3D(x, y, z), spindulys(s) {
+            cout << "Sfera is created" << endl;
+        }
+
+        ~Sfera() {
+            cout << "Sfera is destroyed" << endl;
+        }
+
+        virtual void printInfo() override {
+            cout << "Sfera, spindulys = " << fixed << setprecision(1) << spindulys << endl;
+            printCoordinates();
+        }
+
+        virtual float plotas() const override {
+            return 4 * M_PI * pow(spindulys, 2);
+        }
+
+        virtual float turis() const override {
+            return  4/3 * M_PI * pow(spindulys, 3);
+        }
+};
+
+void spausdintiPlota(const GeomFigura f) {
+    cout << "plotas: " << f.plotas() << endl;
+}
+
+void spausdintiPlota2(const GeomFigura* f) {
+    cout << "plotas: " << f->plotas() << endl;
+}
+
+int main() {
+
+    vector<GeomFigura*> geomFiguros = {
+        new Kvadratas(1.0, 2.0, 5.0),
+        new Skritulys(3.0, 4.0, 6.0),
+        new Kubas(1.0, 2.0, 3.0, 8.0),
+        new Sfera(3.0, 4.0, 5.0, 2.0)
+    };
+
+    cout << "---------------------------" << endl;
+    for (size_t i = 0; i < geomFiguros.size(); i++) {
+        geomFiguros[i]->printInfo();
+        cout << "Plotas = " << fixed << setprecision(2) << geomFiguros[i]->plotas() << endl;
+        cout << "Turis = " << fixed << setprecision(2) << geomFiguros[i]->turis() << endl;
+        cout << "---------------------------" << endl;
+    }
+    cout << "---------------------------" << endl;
+
+    cout << "Funkcija spausdintiPlota:" << endl << endl;
+    for (size_t i = 0; i < geomFiguros.size(); i++) {
+        spausdintiPlota(*geomFiguros[i]);
+        cout << "---------------------------" << endl;
+    }
+    cout << "---------------------------" << endl;
+
+    cout << "Funkcija spausdintiPlota2:" << endl << endl;
+    for (size_t i = 0; i < geomFiguros.size(); i++) {
+        spausdintiPlota2(geomFiguros[i]);
+        cout << "---------------------------" << endl;
+    }
+    cout << "---------------------------" << endl;
+
+    for (size_t i = 0; i < geomFiguros.size(); i++)
+        delete geomFiguros[i];
+}
