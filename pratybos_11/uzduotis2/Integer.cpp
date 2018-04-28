3cc3d19cee54faeb9541079a624da68488abb75e
00riddle00 <tomasgiedraitis@gmail.com>
2018-04-28 18:01:24 +0300

    Add pratybos11 uzduotis2

---
+++ pratybos_11/uzduotis2/Integer.cpp
@@ -0,0 +1,55 @@
+// From "Thinking in C++, 2nd Edition, Volume 1, Annotated Solutions Guide"
+// by Chuck Allison, (c) 2001 MindView, Inc. all rights reserved
+// Available at www.BruceEckel.com.
+// modified by I.M. on 24/10/2008; 19/10/2009
+#include <iostream>
+
+using namespace std;
+
+class Integer {
+	int* p;
+public:
+   Integer(int x = 0) {
+  		p = new int;
+		*p = x;
+        cout << "Integer object is created with x = " << x << endl;
+   }
+
+/*   Integer(const Integer& orig) {*/
+       //p = new int;
+       //*p = *(orig.p);
+        //cout << "Integer object is created by deep copy with x = " << *(orig.p) << endl;
+   //}
+
+  ~Integer() {
+        cout << "Integer object is destroyed with x = " << *p << endl;
+  		delete p;
+    }
+  
+  void set(int x) {*p = x;}
+  int get() const {
+      if (p == nullptr) {
+          // Kodel ne nullptr p yra?
+          cout << "nullptr" << endl;
+      }
+      return *p;
+  }
+
+};
+ 
+void daugybos_lentele(const Integer x){
+	for ( int i = 0; i < 10; i++) {
+		cout << x.get() << " x " << i << " = " << x.get() * i << endl;
+	}
+}
+
+int main() {
+	Integer i1(5);
+	cout << "i1 reiksme yra " << i1.get() << endl;
+
+	daugybos_lentele(i1);
+	cout << "i1 reiksme yra " << i1.get() << endl;
+}
+
+
+
